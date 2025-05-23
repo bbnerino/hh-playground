0c05608cb1f59b3093adb4c71dd1cf27d8b2ba57
uneedcomms-luca <luca.im@uneedcomms.com>
2025-05-23 22:42:23 +0900

    tools 사용 추가

--- src/types/prompts/chat.ts
+++ src/types/prompts/chat.ts
@@ -3,12 +3,6 @@ export class Message {
   role: "user" | "assistant" | "system";
   content?: string;
 
-  // type?: "function_call" | "function_call_output";
-  // call_id?: string;
-  // output?: string;
-  // name?: string;
-  // arguments?: string;
-
   constructor(content: string, role: "user" | "assistant" | "system" = "user") {
     this.role = role;
     this.content = content;
